9de35658fd3be8d3148b3a80ddacc67cd4f7b444
GitHubUser144 <1447652991@qq.com>
2019-05-23 09:04:48 +0800

    测试thymeleaf

--- src/main/java/cn/dazky/SchoolmanagementsystemApplication.java
+++ src/main/java/cn/dazky/SchoolmanagementsystemApplication.java
@@ -1,4 +1,4 @@
-package cn.dazky.schoolmanagementsystem;
+package cn.dazky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/cn/dazky/ServletInitializer.java
+++ src/main/java/cn/dazky/ServletInitializer.java
@@ -1,4 +1,4 @@
-package cn.dazky.schoolmanagementsystem;
+package cn.dazky;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
