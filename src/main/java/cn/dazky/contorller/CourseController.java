497b237c30af9932b54c827865194c2aa256e489
zhong1024 <1374999722@qq.com>
2019-06-03 19:51:53 +0800

    教室和课程信息显示

--- src/main/java/cn/dazky/contorller/CourseController.java
+++ src/main/java/cn/dazky/contorller/CourseController.java
@@ -2,13 +2,17 @@ package cn.dazky.contorller;
 
 import cn.dazky.pojo.DisciplineInfo;
 import cn.dazky.service.Impl.CourseServiceImpl;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 课程信息Course
@@ -19,20 +23,38 @@ import java.util.List;
 @Controller
 public class CourseController {
 
-    @Resource
+    @Autowired
     CourseServiceImpl courseService;
+
+    /**
+     * 跳转到课程管理页面
+     *
+     * @return
+     */
+    @RequestMapping("/staff/ToDisciplineInfoList")
+    public String ToDisciplineInfo() {
+        return "BackgroundManagement/management/AdminCourse";
+    }
 
     /**
      * 查询所有课程信息
      *
      * @return
      */
-    @RequestMapping(value="/selectByExample",method = RequestMethod.GET)
-    public String selectByExample(Model model) {
-        List<DisciplineInfo> disciplineInfoList = courseService.selectByExample();
-        model.addAttribute("courseList",disciplineInfoList);
+    @RequestMapping("/DisciplineInfoList")
+    @ResponseBody
+    public Map<String, Object> DisciplineInfo(HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<String, Object>();
 
-        return "text";
+        PageHelper.startPage(Integer.valueOf(request.getParameter("page")), Integer.valueOf(request.getParameter("limit")));
+        List<DisciplineInfo> disciplineInfoList = courseService.selectByExample();
+        PageInfo<DisciplineInfo> pageInfo = new PageInfo<>(disciplineInfoList);
+
+        map.put("code", 0);
+        map.put("msg", "");
+        map.put("count", pageInfo.getTotal());  //总记录数
+        map.put("data", pageInfo.getList());    //结果集
+        return map;
     }
 
 }
